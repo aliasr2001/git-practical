85a473ab957981c6513bcfb61b501061f49d9c78
Ali Asr <asr.studio2001@gmail.com>
2023-07-03 22:42:11 +0000

    designed the Login and Signup page of auth-app

--- auth_app/src/app/login/page.tsx
+++ auth_app/src/app/login/page.tsx
@@ -1,7 +1,55 @@
-export default function loginPage(){
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
+
+export default function loginPage () {
+    const [ user, setUser ] = React.useState({
+        email: "",
+        password: ""
+    });
+
+    const onLogin = async () => {
+    }
+
+
     return (
-        <div>
-            <h1 className="text-2xl text-red-300 text-center">Login</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-3">
+            <h1 className="text-red-300 text-center text-3xl font-bold">Login</h1>
+            <div className="flex flex-col gap-1 mb-2">
+                {/* its is a email input field */}
+                <label htmlFor="email">Email</label>
+                <input 
+                className="p-2 rounded-lg focus:outline-none bg-transparent border border-gray-700 focus:border-gray-500 mb-3"
+                type="email"
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="Enter the Email"
+                />
+                {/* It is the password field */}
+                <label htmlFor="password">Password</label>
+                <input 
+                className="p-2 rounded-lg focus:outline-none bg-transparent border border-gray-700 focus:border-gray-500 mb-3"
+                type="password"
+                id="password"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+                placeholder="Enter the password"
+                />
+                {/* the signup button starts here */}
+                <button 
+                onClick={onLogin}
+                className="bg-red-400 p-2 hover:bg-red-500 rounded-lg duration-300">Login
+                </button>
+                {/* it the is link to login page */}
+            </div>
+            <p>Don't have an account?</p>
+            <Link href="/signup"
+            className="text-blue-400 hover:text-blue-500"
+            >SignUp Here</Link>
         </div>
     )
 }
--- auth_app/src/app/signup/page.tsx
+++ auth_app/src/app/signup/page.tsx
@@ -17,9 +17,50 @@ export default function signUpPage () {
 
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-red-300 text-center text-3xl">Sign Up</h1>
-            <label htmlFor="username">Username</label>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-3">
+            <h1 className="text-red-300 text-center text-3xl font-bold">Sign Up</h1>
+            <div className="flex flex-col gap-1 mb-2">
+                {/* It is a username input field */}
+                <label htmlFor="username">Username</label>
+                <input 
+                className="p-2 rounded-lg focus:outline-none bg-transparent border border-gray-700 focus:border-gray-500 mb-3"
+                type="text"
+                id="username"
+                value={user.username}
+                onChange={(e) => setUser({...user, username: e.target.value})}
+                placeholder="Enter the Username"
+                />
+                {/* its is a email input field */}
+                <label htmlFor="email">Email</label>
+                <input 
+                className="p-2 rounded-lg focus:outline-none bg-transparent border border-gray-700 focus:border-gray-500 mb-3"
+                type="email"
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="Enter the Email"
+                />
+                {/* It is the password field */}
+                <label htmlFor="password">Password</label>
+                <input 
+                className="p-2 rounded-lg focus:outline-none bg-transparent border border-gray-700 focus:border-gray-500 mb-3"
+                type="password"
+                id="password"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+                placeholder="Enter the password"
+                />
+                {/* the signup button starts here */}
+                <button 
+                onClick={onSignUp}
+                className="bg-red-400 p-2 hover:bg-red-500 rounded-lg duration-300">SignUp
+                </button>
+                {/* it the is link to login page */}
+            </div>
+            <p>Already Signed Up?</p>
+            <Link href="/login"
+            className="text-blue-400 hover:text-blue-500"
+            >Login Here</Link>
         </div>
     )
 }
