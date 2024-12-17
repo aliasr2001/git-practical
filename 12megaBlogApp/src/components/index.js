de42e71ba3257adb13084c0982423151ee025dd6
Ali Asr <asr.studio2001@gmail.com>
2024-12-17 01:43:36 +0000

    Modified Index.js & Header.jsx | Created LogoutBtn.jsx

--- 12megaBlogApp/src/components/index.js
+++ 12megaBlogApp/src/components/index.js
@@ -1,5 +1,7 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
 
-export { Header, Footer, Container };
+export { Header, Footer, Container, LogoutBtn, Logo };
