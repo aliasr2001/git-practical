ab95e079bf3f472567fef221d78235699a7feba7
Ali Asr <asr.studio2001@gmail.com>
2024-12-20 21:10:17 +0000

    fixed the issue in Login.jsx

--- 12megaBlogApp/src/components/Login.jsx
+++ 12megaBlogApp/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState }} from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router'
 import { Button, Input, Logo } from '../components'
