d3e322c291d77f18dc05d82f38c06d417af6a0c5
Ali Asr <asr.studio2001@gmail.com>
2024-12-29 13:28:51 +0000

    Add featured image upload and status selection to PostForm component ::12MegaBlog

--- 12megaBlogApp/src/components/PostForm/PostForm.jsx
+++ 12megaBlogApp/src/components/PostForm/PostForm.jsx
@@ -88,6 +88,33 @@ function PostForm({ post }) {
         />
         <Rte label="Content :" name='content' control={control} defaultValue={getValues('content')}/>
       </div>
+      <div className="w-1/3 px-2">
+          <Input
+              label="Featured Image :"
+              type="file"
+              className="mb-4"
+              accept="image/png, image/jpg, image/jpeg, image/gif"
+              {...register("image", { required: !post })}
+          />
+          {post && (
+              <div className="w-full mb-4">
+                  <img
+                      src={appwriteService.getFilePreview(post.featuredImage)}
+                      alt={post.title}
+                      className="rounded-lg"
+                  />
+              </div>
+          )}
+          <Select
+              options={["active", "inactive"]}
+              label="Status"
+              className="mb-4"
+              {...register("status", { required: true })}
+          />
+          <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+              {post ? "Update" : "Submit"}
+          </Button>
+      </div>
     </form>
   )
 }
